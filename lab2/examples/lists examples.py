0573b241950e22ae5bd9a089a9a37d7a0f7ae660
bekzhanne <137751649+bekzhanne@users.noreply.github.com>
2024-01-25 15:37:09 +0600

    commit

--- lab2/examples/lists examples.py
+++ lab2/examples/lists examples.py
@@ -116,3 +116,117 @@ del thislist[0]
 thislist = ["apple", "banana", "cherry"]
 thislist.clear()
 print(thislist) 
+
+#loop lists
+
+thislist = ["apple", "banana", "cherry"]
+for x in thislist:
+  print(x)
+  
+thislist = ["apple", "banana", "cherry"]
+for i in range(len(thislist)):
+  print(thislist[i])
+  
+thislist = ["apple", "banana", "cherry"]
+i = 0
+while i < len(thislist):
+  print(thislist[i])
+  i = i + 1
+
+thislist = ["apple", "banana", "cherry"]
+[print(x) for x in thislist]
+
+#list comprehension
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = []
+
+for x in fruits:
+  if "a" in x:
+    newlist.append(x)
+
+print(newlist)
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = [x for x in fruits if "a" in x]
+print(newlist)
+
+newlist = [x for x in fruits if x != "apple"]
+
+newlist = [x for x in fruits]
+
+newlist = [x for x in range(10)]
+
+newlist = [x for x in range(10) if x < 5]
+
+newlist = [x.upper() for x in fruits]
+
+newlist = ['hello' for x in fruits]
+
+newlist = [x if x != "banana" else "orange" for x in fruits]
+
+#Sort lists
+
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort()
+print(thislist)
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort()
+print(thislist)
+
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort(reverse = True)
+print(thislist)
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(reverse = True)
+print(thislist)
+
+def myfunc(n):
+  return abs(n - 50)
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(key = myfunc)
+print(thislist)
+
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.sort()
+print(thislist)
+
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.sort(key = str.lower)
+print(thislist)
+
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.reverse()
+print(thislist)
+
+#Copy Lists
+
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+print(mylist)
+
+thislist = ["apple", "banana", "cherry"]
+mylist = list(thislist)
+print(mylist)
+
+#Join Lists
+
+list1 = ["a", "b", "c"]
+list2 = [1, 2, 3]
+list3 = list1 + list2
+print(list3)
+
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+for x in list2:
+  list1.append(x)
+print(list1)
+
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+list1.extend(list2)
+print(list1)
+
