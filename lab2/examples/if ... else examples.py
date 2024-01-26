ed82ecd0b0806c6b6806ac50839f4f1bdd9be750
bekzhanne <137751649+bekzhanne@users.noreply.github.com>
2024-01-26 16:51:14 +0600

    lab2

--- lab2/examples/if ... else examples.py
+++ lab2/examples/if ... else examples.py
@@ -1,15 +1,16 @@
+#example 1
 a = 33
 b = 200
 if b > a:
   print("b is greater than a")
   
-  
+#example 2
 a = 33
 b = 200
 if b > a:
   print("b is greater than a") # you will get an error
 
-
+#example 3
 a = 33
 b = 33
 if b > a:
@@ -17,7 +18,7 @@ if b > a:
 elif a == b:
   print("a and b are equal")
   
-  
+#example 4
 a = 200
 b = 33
 if b > a:
@@ -27,7 +28,7 @@ elif a == b:
 else:
   print("a is greater than b")
   
-  
+#example 5
 a = 200
 b = 33
 if b > a:
@@ -35,40 +36,40 @@ if b > a:
 else:
   print("b is not greater than a")
 
-
+#example 6
 if a > b: print("a is greater than b")
 
-
+#example 7
 a = 2
 b = 330
 print("A") if a > b else print("B")
 
-
+#example 8
 a = 330
 b = 330
 print("A") if a > b else print("=") if a == b else print("B")
 
-
+#example 9
 a = 200
 b = 33
 c = 500
 if a > b and c > a:
   print("Both conditions are True")
   
-  
+#example 10
 a = 200
 b = 33
 c = 500
 if a > b or a > c:
   print("At least one of the conditions is True")
 
-
+#example 11
 a = 33
 b = 200
 if not a > b:
   print("a is NOT greater than b")
 
-
+#example 12
 x = 41
 
 if x > 10:
@@ -78,7 +79,7 @@ if x > 10:
   else:
     print("but not above 20.")
 
-
+#example 13
 a = 33
 b = 200
 
--- lab2/examples/lists examples.py
+++ lab2/examples/lists examples.py
@@ -1,88 +1,113 @@
+#example 1
 mylist = ["apple", "banana", "cherry"]
 
+#example 2
 thislist = ["apple", "banana", "cherry"]
 print(thislist)
 
+#example 3
 thislist = ["apple", "banana", "cherry", "apple", "cherry"]
 print(thislist)
 
+#example 4
 thislist = ["apple", "banana", "cherry"]
 print(len(thislist))
 
+#example 5
 list1 = ["apple", "banana", "cherry"]
 list2 = [1, 5, 7, 9, 3]
 list3 = [True, False, False]
 
+#example 6
 list1 = ["abc", 34, True, 40, "male"]
 
+#example 7
 mylist = ["apple", "banana", "cherry"]
 print(type(mylist))
 
+#example 8
 thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
 print(thislist)
 
 
 
 #Access List Items
+
+#example 1
 thislist = ["apple", "banana", "cherry"]
 print(thislist[1])
 
+#example 2
 thislist = ["apple", "banana", "cherry"]
 print(thislist[-1])
 
+#example 3
 thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
 print(thislist[2:5])
 
+#example 4
 thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
 print(thislist[:4])
 
+#example 5
 thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
 print(thislist[2:])
 
+#example 6
 thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
 print(thislist[-4:-1])
 
+#example 7
 thislist = ["apple", "banana", "cherry"]
 if "apple" in thislist:
   print("Yes, 'apple' is in the fruits list")
   
 #Change List Items
 
+#example 1
 thislist = ["apple", "banana", "cherry"]
 thislist[1] = "blackcurrant"
 print(thislist)
 
+#example 2
 thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
 thislist[1:3] = ["blackcurrant", "watermelon"]
 print(thislist)
 
+#example 3
 thislist = ["apple", "banana", "cherry"]
 thislist[1:2] = ["blackcurrant", "watermelon"]
 print(thislist)
 
+#example 4
 thislist = ["apple", "banana", "cherry"]
 thislist[1:3] = ["watermelon"]
 print(thislist)
 
+#example 5
 thislist = ["apple", "banana", "cherry"]
 thislist.insert(2, "watermelon")
 print(thislist)
 
 #Add List Items
 
+#example 1
 thislist = ["apple", "banana", "cherry"]
 thislist.append("orange")
 print(thislist)
 
+#example 2
 thislist = ["apple", "banana", "cherry"]
 thislist.insert(1, "orange")
 print(thislist)
 
+#example 3
 thislist = ["apple", "banana", "cherry"]
 tropical = ["mango", "pineapple", "papaya"]
 thislist.extend(tropical)
 print(thislist)
 
+#example 4
 thislist = ["apple", "banana", "cherry"]
 thistuple = ("kiwi", "orange")
 thislist.extend(thistuple)
@@ -90,49 +115,60 @@ print(thislist)
 
 #Remove List Items
 
+#example 1
 thislist = ["apple", "banana", "cherry"]
 thislist.remove("banana")
 print(thislist)
 
+#example 2
 thislist = ["apple", "banana", "cherry", "banana", "kiwi"]
 thislist.remove("banana")
 print(thislist)
 
+#example 3
 thislist = ["apple", "banana", "cherry"]
 thislist.pop(1)
 print(thislist)
 
+#example 4
 thislist = ["apple", "banana", "cherry"]
 thislist.pop()
 print(thislist)
 
+#example 5
 thislist = ["apple", "banana", "cherry"]
 del thislist[0]
 print(thislist)
 
+#example 6
 thislist = ["apple", "banana", "cherry"]
 del thislist[0]
 
+#example 7
 thislist = ["apple", "banana", "cherry"]
 thislist.clear()
 print(thislist) 
 
 #loop lists
 
+#example 1
 thislist = ["apple", "banana", "cherry"]
 for x in thislist:
   print(x)
-  
+
+#example 2
 thislist = ["apple", "banana", "cherry"]
 for i in range(len(thislist)):
   print(thislist[i])
-  
+
+#example 3
 thislist = ["apple", "banana", "cherry"]
 i = 0
 while i < len(thislist):
   print(thislist[i])
   i = i + 1
 
+#example 4
 thislist = ["apple", "banana", "cherry"]
 [print(x) for x in thislist]
 
