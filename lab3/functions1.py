80050d9d7168a1afa726d45e159780eaa7cdb0de
bekzhanne <137751649+bekzhanne@users.noreply.github.com>
2024-02-08 08:38:36 +0600

    lab3

---
+++ lab3/functions1.py
@@ -0,0 +1,18 @@
+# #exercise 1
+# def convert(m):
+#     return(m*28.349523125)
+
+# a = int(input()) 
+# print(convert(a))
+
+# #exercise 2
+
+def fahrenheit_to_celsius(fahrenheit):
+    celsius = (5 / 9) * (fahrenheit - 32)
+    return celsius
+
+b = float(input())
+print(fahrenheit_to_celsius(b))
+
+#exercise 3
+def solve(numheads)
